e49720bb76bae740e6e3411a97dcefda243767e9
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2020-11-06 13:49:44 -0800

    edits

--- workflows/lightgbm-iris.py
+++ workflows/lightgbm-iris.py
@@ -19,8 +19,10 @@ script_name = "train-advanced.py"
 environment_file = prefix.joinpath("environments", "lightgbm.txt")
 
 # dataset
-#ds = Dataset.File.from_files("https://github.com/Azure/azureml-examples/blob/main/data/raw/iris/iris.csv")
-ds = Dataset.File.from_files("https://azureopendatastorage.blob.core.windows.net/mlsamples/iris/iris.csv")
+# ds = Dataset.File.from_files("https://github.com/Azure/azureml-examples/blob/main/data/raw/iris/iris.csv")
+ds = Dataset.File.from_files(
+    "https://azureopendatastorage.blob.core.windows.net/mlsamples/iris/iris.csv"
+)
 
 # azure ml settings
 environment_name = "lightgbm"
