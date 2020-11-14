a03ceb86539c31fcd0ae4d5ce6921755a3ca07df
lostmygithubaccount <cody.dkdc2@gmail.com>
2020-11-13 18:29:17 -0800

    minor edits

--- workflows/basic/job.py
+++ workflows/basic/job.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from azureml.core import Workspace, ScriptRunConfig, Experiment, Environment, Dataset
 
 # constants
+compute_name = "cpu-cluster"  # use "local" for local training
+entry_script = "train.py"
 environment_name = "myenv-template"
 experiment_name = "template-workflow-base"
-compute_name = "cpu-cluster"
 data_uri = "https://azuremlexamples.blob.core.windows.net/datasets/iris.csv"
 
 # get workspace
@@ -14,23 +15,20 @@ ws = Workspace.from_config()
 # setup path prefix
 prefix = Path(__file__).parent
 
-# training script
+# get relative paths
 script_dir = str(prefix.joinpath("src"))
-script_name = "train.py"
-
-# environment file
 environment_file = str(prefix.joinpath("requirements.txt"))
 
-# dataset
+# create dataset
 ds = Dataset.File.from_files(data_uri)
 
 # create environment
 env = Environment.from_pip_requirements(environment_name, environment_file)
 
-# setup arguments
+# setup entry script arguments
 args = ["--data-dir", ds.as_mount()]
 
-# create job config
+# create a job configuration
 src = ScriptRunConfig(
     source_directory=script_dir,
     script=script_name,
@@ -39,6 +37,6 @@ src = ScriptRunConfig(
     compute_target=compute_name,
 )
 
-# submit job
+# run the job
 run = Experiment(ws, experiment_name).submit(src)
 run.wait_for_completion(show_output=True)
